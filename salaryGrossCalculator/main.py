f6cbb6842d9d34df0cea2b3d115e566e124747e8
Jan <kleta.jan@outlook.com>
2023-02-23 07:12:17 +0800

    Update salaryGrossCalculator
    adding overtime bonus and block catch to prevent user from typing different value than integer

--- salaryGrossCalculator/main.py
+++ salaryGrossCalculator/main.py
@@ -1,4 +1,16 @@
+
+#Rewrite your pay computation to give the employee 1.5 times the hourly rate for hours worked above 40 hours.
+#Rewrite your pay program using try and except so that your program handles non-numeric input gracefully by printing a message and exiting the program. The following shows two executions of the program:
 hours = input("How many hours have you worked?")
 rates = input("how much do you earn per hour?")
-pay = float(hours) * float(rates)
+try:
+    fhours = float(hours)
+    frates = float(rates)
+except:
+    print("Please, enter numeric input")
+    quit()
+if fhours > 40:
+    pay = fhours * frates + (fhours - 40) * frates * 0.5
+else:
+    pay = fhours * frates
 print("You earn: ", pay)
