93bd24d143a53129e5831ba0dbb49bea0506455e
Jan <kleta.jan@outlook.com>
2023-03-02 18:19:01 +0800

    adding more function to prob_calculator.py

--- Probability-Calculator/prob_calculator.py
+++ Probability-Calculator/prob_calculator.py
@@ -2,15 +2,26 @@ import copy
 import random
 # Consider using the modules imported above.
 
+
 class Hat():
+
   def __init__(self, listOfBalls):
     self.contest = []
-    
-    self.contest.append(ball)
-  
-  
+
+    counter = 0
+
+    for ball in listOfBalls:
+      ballColor = listOfBalls[counter].split('=')[0]
+      ballCount = int(listOfBalls[counter].split('=')[1])
+      while ballCount > 0:
+        self.contest(ballColor)
+        ballCount = ballCount - 1
+      counter = counter + 1
+
   def draw():
     return ""
 
 
 def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
+  m = 0
+  return m / num_experiments
--- Probability-Calculator/prod.py
+++ Probability-Calculator/prod.py
@@ -1,8 +1,14 @@
-import copy
-import random
-# Consider using the modules imported above.
+listOfBalls = ("black=2", "red=1", "green=3")
 
-class Hat:
-
-
-def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
+newList = []
+counter = 0
+for ball in listOfBalls:
+    ballColor = listOfBalls[counter].split('=')[0]
+    ballCount = int(listOfBalls[counter].split('=')[1])
+    while ballCount > 0:
+        newList.append(ballColor)
+        ballCount = ballCount - 1
+    counter = counter + 1
+        
+print(newList)
+print('test: "black", "black", "red", "green", "green", "green"') 
