54eb4c8fa74f6651604bd9a8a78f9219d93e3c7b
Jan <kleta.jan@outlook.com>
2023-03-03 16:06:09 +0800

    fix: change init method and big update Draw and experiment in prob_calculator

--- Probability-Calculator/prob_calculator.py
+++ Probability-Calculator/prob_calculator.py
@@ -5,23 +5,34 @@ import random
 
 class Hat():
 
-  def __init__(self, listOfBalls):
+  def __init__(self, **listOfBalls):
     self.contest = []
+    for colorBall, countBall in listOfBalls.items():
+      for balls in range(countBall):
+        self.contents.append(colorBall)
 
-    counter = 0
-
-    for ball in listOfBalls:
-      ballColor = listOfBalls[counter].split('=')[0]
-      ballCount = int(listOfBalls[counter].split('=')[1])
-      while ballCount > 0:
-        self.contest(ballColor)
-        ballCount = ballCount - 1
-      counter = counter + 1
-
-  def draw():
-    return ""
+  def draw(self, num_balls_drawn):
+    if num_balls_drawn <= len(self.contents):
+      drawn_balls = []
+      for i in range(num_balls_drawn):
+        random_int = random.randrange(len(self.contents))
+        drawn_balls.append(self.contents[random_int])
+        self.contents.pop(random_int)
+      return drawn_balls
+    else:
+      return self.contents
 
 
 def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
   m = 0
+  listExpected = []
+  for colorBallExpected, countBallExpected in expected_balls.items():
+    for ballsExpected in range(countBallExpected):
+      listExpected.append(colorBallExpected)
+  for num_ball_drawn in range(num_balls_drawn):
+    drawNumBallsDrawn = hat.draw.(num_ball_drawn)
+    
+    
+    
+    
   return m / num_experiments
--- Probability-Calculator/prod.py
+++ Probability-Calculator/prod.py
@@ -12,3 +12,9 @@ for ball in listOfBalls:
         
 print(newList)
 print('test: "black", "black", "red", "green", "green", "green"') 
+
+def total_fruits(**kwargs):
+    print(kwargs, type(kwargs))
+
+
+total_fruits(banana=5, mango=7, apple=8)
