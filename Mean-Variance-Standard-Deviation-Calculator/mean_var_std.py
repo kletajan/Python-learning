889bf0de73bf91350c1843cc14c11e0956987f97
Jan <kleta.jan@outlook.com>
2023-03-17 21:12:00 +0800

    feat: adding filter to function in calc

--- Mean-Variance-Standard-Deviation-Calculator/mean_var_std.py
+++ Mean-Variance-Standard-Deviation-Calculator/mean_var_std.py
@@ -1,19 +1,30 @@
 import numpy as np
 
 def calculate(list):
-  x = np.array(list).reshape(3,3)
   
   if len(list) == 9:
-      calculations = {
-  'mean': meanList,
-  'variance': varianceList,
-  'standard deviation': standevList,
-  'max': mAXList,
-  'min': minList,
-  'sum': sumList
+    x = np.array(list).reshape(3,3)
+
+    meanList = [np.mean(x, axis=0, dtype=np.float32), x.mean(axis=1), x.mean()]
+    varianceList = 5
+    standevList = 5
+    maxList = 5
+    minList = 5
+    sumList = 5
+
+    calculations = {
+      'mean': meanList,
+      'variance': varianceList,
+      'standard deviation': standevList,
+      'max': maxList,
+      'min': minList,
+      'sum': sumList
     }
 
   else:
-      calculations = "List must contain nine numbers" 
+    calculations = ValueError("List must contain nine numbers")
+    
+  print(calculations)
 
-    return calculations
+
+calculate([0,1,2,3,4,5,6,7,8])
